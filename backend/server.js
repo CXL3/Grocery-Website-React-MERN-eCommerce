ea902cfc2082471a824dd87c676ade6c226dd2e9
Claire Lu <appsfunlu@gmail.com>
2021-11-14 00:45:00 -0800

    ES Models

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,29 @@
-const express = require('express');
-const products = require('./data/products')
+import express from  'express'
+import dotenv from  'dotenv'
+import products from './data/products.js'
 
-const app = express();
+dotenv.config()
+
+const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello, it is working.');
+  res.send('Hello, it is working.')
 })
 
 app.get('/api/products', (req, res) => {
-    //transfer it into json data type
-    res.json(products)
+  //transfer it into json data type
+  res.json(products)
 })
 
 app.get('/api/products/:id', (req, res) => {
-    //get the single product that matches the products/:id
-    const product = products.find(product => product._id === req.params.id)
-    res.json(product)
+  //get the single product that matches the products/:id
+  const product = products.find((product) => product._id === req.params.id)
+  res.json(product)
 })
 
-app.listen(5000, console.log('Server listening on port 5000'));
+const PORT = process.env.PORT || 5000
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
