a342810c47117538b0eaf840ce55fc96df3bc450
Claire Lu <appsfunlu@gmail.com>
2021-11-14 23:43:56 -0800

    setup MongoDB and Mongoose

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+
+const connectDB = async () => {
+  try {
+    //use async await because mongoose.connect is always returning a promise
+    const conn = await mongoose.connect(process.env.MONGO_URI)
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`)
+  } catch (error) {
+    console.log(`Error:${error.message}`)
+    //exit with failure
+    process.exit(1)
+  }
+}
+export default connectDB
