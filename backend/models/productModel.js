fa7e71e6a83c8e1d595f91f6da67d33042f9719d
Claire Lu <appsfunlu@gmail.com>
2021-11-16 23:07:54 -0800

    set up models

---
+++ backend/models/productModel.js
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose'
+
+const productSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    source: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const Product = mongoose.model('Product', productSchema)
+
+export default Product
